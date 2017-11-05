65ec51edc3ce0a48c496599dd3a7e86604d1fb61
prince <princexavier1996@gmal.com>
2017-11-05 14:42:03 +0530

    JSON DOM manipulation

---
+++ js/json.js
@@ -0,0 +1,360 @@
+$(function() {
+        var object = [{
+            "id": 3,
+            "username": "krishna@billiontags.com",
+            "first_name": "Krishna",
+            "last_name": "Madhavan",
+            "userprofile": {
+                "id": 2,
+                "assembly": {
+                    "id": 2,
+                    "parliament": {
+                        "id": 1,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Thiruvallur"
+                    },
+                    "assembly": "Avadi"
+                },
+                "activation_key": "a04d52704805803c4dc33442bdd22788",
+                "gender": "male",
+                "date_of_birth": "1994-12-12",
+                "profile_pic": "http://democracy.zordec.com/media/14731385_1660856620911293_3761623978301411029_n_tplzYkr.jpg",
+                "address": "#23,Ram Nagar,5th Street,Padapai,Chennai-48.",
+                "contact_number": "9840525653",
+                "is_contact_public": true,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": true,
+                "is_activated": true,
+                "user": 3,
+                "role": 2
+            }
+        }, {
+            "id": 4,
+            "username": "priyanka@billiontags.com",
+            "first_name": "Priyanka",
+            "last_name": "K",
+            "userprofile": {
+                "id": 3,
+                "assembly": {
+                    "id": 2,
+                    "parliament": {
+                        "id": 1,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Thiruvallur"
+                    },
+                    "assembly": "Avadi"
+                },
+                "activation_key": "646964",
+                "gender": "female",
+                "date_of_birth": "1995-08-15",
+                "profile_pic": "http://democracy.zordec.com/media/2015-08-12_09-17-12.641.jpg",
+                "address": "#45,Lakshmi Appt,Kashmin Nagar,Madipakkam,Chennai-67.",
+                "contact_number": "9840526355",
+                "is_contact_public": false,
+                "is_email_public": true,
+                "is_profile_pic_public": false,
+                "is_address_public": false,
+                "is_moderator": true,
+                "is_activated": false,
+                "user": 4,
+                "role": 3
+            }
+        }, {
+            "id": 5,
+            "username": "gladwin@billiontags.com",
+            "first_name": "Gladwin",
+            "last_name": "Henald",
+            "userprofile": {
+                "id": 4,
+                "assembly": {
+                    "id": 3,
+                    "parliament": {
+                        "id": 2,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Chennai North"
+                    },
+                    "assembly": "Perambur"
+                },
+                "activation_key": "909170",
+                "gender": "male",
+                "date_of_birth": "1994-12-25",
+                "profile_pic": null,
+                "address": "#4,RSZ Plaza,Henri Nagar,Chithoor,Andhra-78.",
+                "contact_number": "9840587878",
+                "is_contact_public": false,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": false,
+                "is_activated": false,
+                "user": 5,
+                "role": 3
+            }
+        }, {
+            "id": 6,
+            "username": "amirtha@billiontags.com",
+            "first_name": "Amirtha",
+            "last_name": "S",
+            "userprofile": {
+                "id": 5,
+                "assembly": {
+                    "id": 3,
+                    "parliament": {
+                        "id": 2,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Chennai North"
+                    },
+                    "assembly": "Perambur"
+                },
+                "activation_key": "4fy-c6fa46e2c1ee3f25492c",
+                "gender": "male",
+                "date_of_birth": "1994-05-11",
+                "profile_pic": null,
+                "address": "#6/23,Ranganathapuram,5th Street,W-Tambaram,Chennai-45.",
+                "contact_number": "9840589663",
+                "is_contact_public": false,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": false,
+                "is_activated": false,
+                "user": 6,
+                "role": 3
+            }
+        }, {
+            "id": 7,
+            "username": "pravintenna@gmail.com",
+            "first_name": "Pravin",
+            "last_name": "P",
+            "userprofile": {
+                "id": 6,
+                "assembly": {
+                    "id": 8,
+                    "parliament": {
+                        "id": 4,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Chennai Central"
+                    },
+                    "assembly": "Anna Nagar"
+                },
+                "activation_key": "42b3eeb30e0200138f39427f810fd6a4",
+                "gender": "male",
+                "date_of_birth": "1988-07-13",
+                "profile_pic": null,
+                "address": "#6/23,Ranganathapuram,5th Street,W-Tambaram,Chennai-45.",
+                "contact_number": "9840569885",
+                "is_contact_public": false,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": true,
+                "is_activated": true,
+                "user": 7,
+                "role": 3
+            }
+        }, {
+            "id": 8,
+            "username": "gopiakshay110595@gmail.com",
+            "first_name": "Gopi",
+            "last_name": "P",
+            "userprofile": {
+                "id": 7,
+                "assembly": {
+                    "id": 7,
+                    "parliament": {
+                        "id": 4,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Chennai Central"
+                    },
+                    "assembly": "Egmore"
+                },
+                "activation_key": "724833a11259c1bdcf01564f41cf917a",
+                "gender": "male",
+                "date_of_birth": "1995-05-11",
+                "profile_pic": "http://democracy.zordec.com/media/img.jpg",
+                "address": "#6/23,Ranganathapuram,5th Street,W-Tambaram,Chennai-45.",
+                "contact_number": "9094483145",
+                "is_contact_public": true,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": true,
+                "is_activated": true,
+                "user": 8,
+                "role": 2
+            }
+        }, {
+            "id": 9,
+            "username": "praveen.mugay@gmail.com",
+            "first_name": "Praveen",
+            "last_name": "Kumar",
+            "userprofile": {
+                "id": 8,
+                "assembly": {
+                    "id": 2,
+                    "parliament": {
+                        "id": 1,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Thiruvallur"
+                    },
+                    "assembly": "Avadi"
+                },
+                "activation_key": "2345979db27b7c1c222136f6ee59fa64",
+                "gender": "male",
+                "date_of_birth": "1992-06-22",
+                "profile_pic": "http://democracy.zordec.com/media/WP_20161007_13_28_58_Selfie_3_ERgV4jq.jpg",
+                "address": "Kilpauk Garden, Chennai",
+                "contact_number": "9095985865",
+                "is_contact_public": true,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": false,
+                "is_activated": true,
+                "user": 9,
+                "role": 3
+            }
+        }, {
+            "id": 11,
+            "username": "praveen@gmail.com",
+            "first_name": "Praveen",
+            "last_name": "Billiontags",
+            "userprofile": {
+                "id": 9,
+                "assembly": {
+                    "id": 2,
+                    "parliament": {
+                        "id": 1,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Thiruvallur"
+                    },
+                    "assembly": "Avadi"
+                },
+                "activation_key": "4g2-beb8723de2328c3ff152",
+                "gender": "male",
+                "date_of_birth": "1990-08-11",
+                "profile_pic": null,
+                "address": "#54,Ram nagar,W-Mambalam,Chennai-600045",
+                "contact_number": "9840589656",
+                "is_contact_public": false,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": false,
+                "is_activated": false,
+                "user": 11,
+                "role": 3
+            }
+        }, {
+            "id": 12,
+            "username": "arun2228lava@gmail.com",
+            "first_name": "Arun",
+            "last_name": "Kumar",
+            "userprofile": {
+                "id": 10,
+                "assembly": {
+                    "id": 2,
+                    "parliament": {
+                        "id": 1,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Thiruvallur"
+                    },
+                    "assembly": "Avadi"
+                },
+                "activation_key": "b834a0771a825aae5214cee5a2f91508",
+                "gender": "male",
+                "date_of_birth": "1993-10-21",
+                "profile_pic": null,
+                "address": "28 Iyyapillai garden 2nd street",
+                "contact_number": "9962620218",
+                "is_contact_public": false,
+                "is_email_public": false,
+                "is_profile_pic_public": false,
+                "is_address_public": false,
+                "is_moderator": true,
+                "is_activated": true,
+                "user": 12,
+                "role": 3
+            }
+        }, {
+            "id": 13,
+            "username": "arun@billiontags.com",
+            "first_name": "Arun",
+            "last_name": "Kumar",
+            "userprofile": {
+                "id": 11,
+                "assembly": {
+                    "id": 8,
+                    "parliament": {
+                        "id": 4,
+                        "state": {
+                            "id": 1,
+                            "state": "Tamil Nadu"
+                        },
+                        "parliament": "Chennai Central"
+                    },
+                    "assembly": "Anna Nagar"
+                },
+                "activation_key": "4gd-f3d12373f15775d2929e",
+                "gender": "male",
+                "date_of_birth": "2016-10-22",
+                "profile_pic": null,
+                "address": "address",
+                "contact_number": "9962620218",
+                "is_contact_public": false,
+                "is_email_public": false,
+                "is_profile_pic_public": true,
+                "is_address_public": true,
+                "is_moderator": false,
+                "is_activated": false,
+                "user": 13,
+                "role": 3
+            }
+        }]
+
+
+
+
+
+
+
+
+
+        for (i = 0; i < object.length; i++) {
+            $("#data").append('<h1>User' + (i + 1) + ' </h1><div class ="col-lg-4"><p>Email:' + (object[i].username) + '</p> </div><div class ="col-lg-4"><p>Name:' + (object[i].first_name) + ' ' + (object[i].last_name) + '<p> </div><div class ="col-lg-4"><p>parliament:' + (object[i].userprofile.assembly.parliament.parliament) + '</p> </div>');
+
+        }
+
+
+
+
+    });
